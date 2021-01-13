d1293d86b665bac8d9b2a71a773c2ff960be8eac
pjchender <aaronchen@jubo.health>
2021-01-13 13:50:22 +0800

    fix: filter and contain methods

--- helpers/slice/slice.go
+++ helpers/slice/slice.go
@@ -2,9 +2,9 @@
 package slice
 
 // Contains will check whether searchElement contains the target
-func Contains(searchElement []int, target int) bool {
+func Contains(searchElement []*int, target int) bool {
 	for _, element := range searchElement {
-		if element == target {
+		if *element == target {
 			return true
 		}
 	}
@@ -12,7 +12,7 @@ func Contains(searchElement []int, target int) bool {
 }
 
 // Filter will check whether searchElement fulfill the handler condition
-func Filter(searchElement []int, handler func(int) bool) []int {
+func Filter(searchElement []*int, handler func(*int) bool) []*int {
 	n := 0
 	for _, element := range searchElement {
 		if handler(element) {
--- helpers/slice/slice_test.go
+++ helpers/slice/slice_test.go
@@ -9,24 +9,25 @@ import (
 
 func ExampleContains() {
 	s := []int{1, 3, 5}
-	fmt.Println("isContain 5", Contains(s, 5))
+	fmt.Println("isContain 5", Contains(toIntPointers(s), 5))
 	// Output: isContain 5 true
 
-	fmt.Println("isContain 6", Contains(s, 6))
+	fmt.Println("isContain 6", Contains(toIntPointers(s), 6))
 	// Output: isContain 6 false
 }
 
 func TestContains(t *testing.T) {
 	s := []int{1, 3, 5}
 
-	assert.True(t, Contains(s, 5))
-	assert.False(t, Contains(s, 6))
+	assert.True(t, Contains(toIntPointers(s), 5))
+	assert.False(t, Contains(toIntPointers(s), 6))
 }
 
 func ExampleFilter() {
 	s := []int{1, 3, 5}
-	biggerThenFive := Filter(s, func(i int) bool {
-		return i > 2
+	biggerThenFive := Filter(toIntPointers(s), func(i *int) bool {
+		number := *i
+		return number > 2
 	})
 	fmt.Println(biggerThenFive)
 	// Output: [3, 4]
@@ -34,12 +35,13 @@ func ExampleFilter() {
 
 func TestFilter(t *testing.T) {
 	s := []int{1, 3, 5}
-	biggerThenFive := Filter(s, func(i int) bool {
-		return i > 2
+	biggerThenFive := Filter(toIntPointers(s), func(i *int) bool {
+		number := *i
+		return number > 2
 	})
 
-	expect := []int{3, 4}
-	assert.Equal(t, expect, biggerThenFive)
+	expect := []int{3, 5}
+	assert.Equal(t, expect, toIntValues(biggerThenFive))
 }
 
 func ExampleFilterWithContain() {
@@ -47,8 +49,9 @@ func ExampleFilterWithContain() {
 	exist := []int{2, 4, 6}
 	update := []int{1, 3, 6}
 
-	filtered := Filter(exist, func(i int) bool {
-		return !Contains(update, i)
+	filtered := Filter(toIntPointers(exist), func(i *int) bool {
+		number := *i
+		return !Contains(toIntPointers(update), number)
 	})
 	fmt.Println(filtered)
 	// Output: [2, 4]
@@ -58,10 +61,37 @@ func TestFilterWithContain(t *testing.T) {
 	exist := []int{2, 4, 6}
 	update := []int{1, 3, 6}
 
-	filtered := Filter(exist, func(i int) bool {
-		return !Contains(update, i)
+	filtered := Filter(toIntPointers(exist), func(i *int) bool {
+		number := *i
+		return !Contains(toIntPointers(update), number)
 	})
 
 	expect := []int{2, 4}
-	assert.Equal(t, expect, filtered)
+	assert.Equal(t, expect, toIntValues(filtered))
+}
+
+func toIntPointer(number int) *int {
+	return &number
+}
+
+func toIntValue(intPoint *int) int {
+	return *intPoint
+}
+
+func toIntPointers(intValues []int) []*int {
+	intPointers := make([]*int, len(intValues))
+	for i := range intValues {
+		intPointers[i] = &intValues[i]
+	}
+
+	return intPointers
+}
+
+func toIntValues(intPoints []*int) []int {
+	intValues := make([]int, len(intPoints))
+	for i := range intPoints {
+		intValues[i] = *intPoints[i]
+	}
+
+	return intValues
 }
